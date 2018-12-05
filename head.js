1b31f78121d67c0bf2cb080391705c5b07c83ce7
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-12-05 13:10:39 +0530

    Initial Commit for main file.

--- head.js
+++ head.js
@@ -1,18 +1,12 @@
-/* 
-  Usage:
-  node ./head.js file1
-  node ./head.js -n5 file1
-  node ./head.js -n 5 file1
-  node ./head.js -5 file1
-  node ./head.js file1 file2
-  node ./head.js -n 5 file1 file2
-  node ./head.js -n5 file1 file2
-  node ./head.js -5 file1 file2 
-  node ./head.js -c5 file1
-  node ./head.js -c 5 file1
-  node ./head.js -c5 file1 file2
-  node ./head.js -c 5 file1 file2
-*/
+const {
+  organiseInputs 
+} = require('./src/lib.js');
 
+const readFile = require('fs').readFileSync;
 
+const main = function(args){
+  const headDetails = organiseInputs(args);
+  return headDetails;
+}
 
+console.log(main(process.argv.slice(2)));
