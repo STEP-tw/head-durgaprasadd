694d8616de94dd3edec7253992b32bdd38d5ab4d
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-12-07 15:41:04 +0530

    All files are prettified.

--- src/lib.js
+++ src/lib.js
@@ -1,79 +1,88 @@
-const isCharacterType = function(string){
-  let firstTwoCharacters = string.slice(0,2);
+const isCharacterType = function(string) {
+  let firstTwoCharacters = string.slice(0, 2);
   return firstTwoCharacters == '-c';
-}
+};
 
-const findType = function(arg){
-  if(isCharacterType(arg)){
+const findType = function(arg) {
+  if (isCharacterType(arg)) {
     return 'c';
   }
   return 'n';
-}
+};
 
-const findRange = function(firstArg,SecondArg){
+const findRange = function(firstArg, SecondArg) {
   let range;
-  if(firstArg[0] == '-'){
+  if (firstArg[0] == '-') {
     range = firstArg.slice(1);
   }
-  if(firstArg.slice(0,2) == '-c' || firstArg.slice(0,2) == '-n'){
+  if (firstArg.slice(0, 2) == '-c' || firstArg.slice(0, 2) == '-n') {
     range = firstArg.slice(2) || SecondArg;
   }
   return range || 10;
-}
+};
 
-const isFile = function(string){
-  let isType = string[0]=='-';
+const isFile = function(string) {
+  let isType = string[0] == '-';
   let isNumber = +string;
   return !isType && !isNumber;
-}
+};
 
-const findFileNames = function(args){
+const findFileNames = function(args) {
   let fileNames = args.filter(isFile);
   return fileNames;
-}
+};
 
-const organiseInputs = function(args){
+const organiseInputs = function(args) {
   let type = findType(args[0]);
-  let range = findRange(args[0],args[1]);
+  let range = findRange(args[0], args[1]);
   let fileNames = findFileNames(args);
-  return { type, range, fileNames }
-}
+  return {type, range, fileNames};
+};
 
-const getSelectedData = function(type,range,content){
-  let delimiter = { c:'',n:'\n'};
+const getSelectedData = function(type, range, content) {
+  let delimiter = {c: '', n: '\n'};
   let data = content.split(delimiter[type]);
-  return data.slice(0,range).join(delimiter[type]);
-}
+  return data.slice(0, range).join(delimiter[type]);
+};
 
-const isExist = function(existsFile,fileName){
+const isExist = function(existsFile, fileName) {
   return existsFile(fileName);
-}
+};
 
-const getOutput = function(readFile,existsFile,{type,range},result,fileName){
-  if(isExist(existsFile,fileName)){
-    let content = readFile(fileName,'utf-8');
-    let data = getSelectedData(type,range,content);
-    let headline = '==> '+fileName+' <==';
-    result.push(headline+'\n'+data+'\n');
+const getOutput = function(
+  readFile,
+  existsFile,
+  {type, range},
+  result,
+  fileName,
+) {
+  if (isExist(existsFile, fileName)) {
+    let content = readFile(fileName, 'utf-8');
+    let data = getSelectedData(type, range, content);
+    let headline = '==> ' + fileName + ' <==';
+    result.push(headline + '\n' + data + '\n');
     return result;
   }
-  let message = 'head: '+fileName+': No such file or directory';
+  let message = 'head: ' + fileName + ': No such file or directory';
   result.push(message);
   return result;
-}
+};
 
-const head = function(args,readFile,existsFile){
-  let { type, range, fileNames } = organiseInputs(args);
-  let message = {c:'byte',n:'line'};
-  if(!(+range > 0)){
-    return 'head: illegal '+message[type]+' count -- '+range;
+const head = function(args, readFile, existsFile) {
+  let {type, range, fileNames} = organiseInputs(args);
+  let message = {c: 'byte', n: 'line'};
+  if (!(+range > 0)) {
+    return 'head: illegal ' + message[type] + ' count -- ' + range;
   }
-  let output = fileNames.reduce(getOutput.bind(null,readFile,existsFile,{type,range}),[]);
-  if(output.length == 1 && isExist(existsFile,fileNames[0])){
-    output = output[0].split('\n').slice(1)
+  let output = fileNames.reduce(
+    getOutput.bind(null, readFile, existsFile, {type, range}),
+    [],
+  );
+  if (output.length == 1 && isExist(existsFile, fileNames[0])) {
+    output = output[0].split('\n').slice(1);
   }
   return output.join('\n');
-}
+};
 
 module.exports = {
   isCharacterType,
@@ -83,5 +92,5 @@ module.exports = {
   isFile,
   findFileNames,
   getSelectedData,
-  head
-}
+  head,
+};
