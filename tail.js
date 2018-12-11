41eb649cd8e2dfd4eecda03bf93546f4cafe59ff
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-12-11 15:49:08 +0530

    Removed unwanted text in tail.js

--- tail.js
+++ tail.js
@@ -1,18 +1,3 @@
-/* 
-  Usage:
-  node ./tail.js file1
-  node ./tail.js -n5 file1
-  node ./tail.js -n 5 file1
-  node ./tail.js -5 file1
-  node ./tail.js file1 file2
-  node ./tail.js -n 5 file1 file2
-  node ./tail.js -n5 file1 file2
-  node ./tail.js -5 file1 file2 
-  node ./tail.js -c5 file1
-  node ./tail.js -c 5 file1
-  node ./tail.js -c5 file1 file2
-  node ./tail.js -c 5 file1 file2
-*/
 const { tail } = require("./src/lib.js");
 const { readFileSync, existsSync } = require("fs");
 console.log(tail(process.argv.slice(2), readFileSync, existsSync));
