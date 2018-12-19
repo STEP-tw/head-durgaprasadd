ace63022f4beee0942ecf09e851ef038d6002a89
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-12-19 18:04:51 +0530

    Introduced errorMessageForIllegalCount function.

--- src/lib.js
+++ src/lib.js
@@ -12,6 +12,15 @@ const getSelectedData = function(option, range, content, command) {
 const addHeader = function(fileName) {
   return `==> ${fileName} <==`;
 };
+
+const errorMessageForIllegalCount = function(command, option, range) {
+  const message = {
+    head: { byte: "byte count", line: "line count" },
+    tail: { byte: "offset", line: "offset" }
+  };
+  return `${command}: illegal ${message[command][option]} -- ${range}`;
+};
+
 const getOutput = function(fs, command, option, range, fileName) {
   if (fs.existsSync(fileName)) {
     let content = fs.readFileSync(fileName, "utf-8");
@@ -24,14 +33,13 @@ const getOutput = function(fs, command, option, range, fileName) {
 
 const generateOutput = function(args, fs, command = "head") {
   let { option, range, fileNames } = parse(args);
-  let message = {
-    head: { byte: "byte count", line: "line count" },
-    tail: { byte: "offset", line: "offset" }
-  };
+
   if (range <= 0 || isNaN(range)) {
-    return command + ": illegal " + message[command][option] + " -- " + range;
+    return errorMessageForIllegalCount(command, option, range);
   }
+
   let output = fileNames.map(getOutput.bind(null, fs, command, option, range));
+
   if (output.length == 1 && fs.existsSync(fileNames[0])) {
     output = output[0].split("\n").slice(1);
   }
@@ -50,5 +58,6 @@ module.exports = {
   getSelectedData,
   tail,
   generateOutput,
-  addHeader
+  addHeader,
+  errorMessageForIllegalCount
 };
--- test/libTest.js
+++ test/libTest.js
@@ -4,7 +4,8 @@ const {
   getSelectedData,
   tail,
   generateOutput,
-  addHeader
+  addHeader,
+  errorMessageForIllegalCount
 } = require("../src/lib.js");
 
 const fileContents = {
@@ -130,5 +131,41 @@ describe("addHeader", () => {
     let actual = addHeader("file");
     let expected = "==> file <==";
     assert.deepEqual(actual, expected);
+  });
+});
+
+describe("errorMessageForIllegalCount", () => {
+  describe("for head", () => {
+    it("should return error message for illegal line count", () => {
+      let actual = errorMessageForIllegalCount("head", "line", "-10");
+      let expected = "head: illegal line count -- -10";
+      assert.deepEqual(actual, expected);
+
+      actual = errorMessageForIllegalCount("head", "line", "2x");
+      expected = "head: illegal line count -- 2x";
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return error message for illegal byte count ", () => {
+      let actual = errorMessageForIllegalCount("head", "byte", "-3");
+      let expected = "head: illegal byte count -- -3";
+      assert.deepEqual(actual, expected);
+
+      actual = errorMessageForIllegalCount("head", "byte", "3e");
+      expected = "head: illegal byte count -- 3e";
+      assert.deepEqual(actual, expected);
+    });
+  });
+
+  describe("for tail", () => {
+    it("should return error message for illegal line and byte count", () => {
+      let actual = errorMessageForIllegalCount("tail", "line", "-3");
+      let expected = "tail: illegal offset -- -3";
+      assert.deepEqual(actual, expected);
+
+      actual = errorMessageForIllegalCount("tail", "byte", "3s");
+      expected = "tail: illegal offset -- 3s";
+      assert.deepEqual(actual, expected);
+    });
   });
 });
